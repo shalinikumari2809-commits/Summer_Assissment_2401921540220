608c5e97dd006886d49bd8b07b1a1e5075a315f2
shalini kumari <shalinikumari2898@gmail.com>
2026-06-05 20:41:56 +0530

    Update maximum avg subarray1.cpp

--- week1/day_2/maximum avg subarray1.cpp
+++ week1/day_2/maximum avg subarray1.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int n = nums.size();
 
+        long long sum = 0;
+
+        // First window
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+
+        long long maxSum = sum;
+
+        // Sliding window
+        for (int i = k; i < n; i++) {
+            sum += nums[i] - nums[i - k];
+            maxSum = max(maxSum, sum);
+        }
+
+        return (double)maxSum / k;
+    }
+};
