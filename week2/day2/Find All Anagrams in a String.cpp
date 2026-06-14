96c87c0715a2f2ca6ec8e1688a454615f32f429b
shalini kumari <shalinikumari2898@gmail.com>
2026-06-14 17:57:29 +0530

    Update Find All Anagrams in a String.cpp

--- week2/day2/Find All Anagrams in a String.cpp
+++ week2/day2/Find All Anagrams in a String.cpp
@@ -1,1 +1,31 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> ans;
 
+        int n = s.size();
+        int m = p.size();
+
+        if (m > n) return ans;
+
+        vector<int> pCount(26, 0);
+        vector<int> winCount(26, 0);
+
+        for (int i = 0; i < m; i++) {
+            pCount[p[i] - 'a']++;
+            winCount[s[i] - 'a']++;
+        }
+
+        if (pCount == winCount)
+            ans.push_back(0);
+        for (int i = m; i < n; i++) {
+            winCount[s[i] - 'a']++;      
+            winCount[s[i - m] - 'a']--;   
+
+            if (pCount == winCount)
+                ans.push_back(i - m + 1);
+        }
+
+        return ans;
+    }
+};
