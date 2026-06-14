5880e1f2300b6de2d93a9377f6f740b48d528310
shalini kumari <shalinikumari2898@gmail.com>
2026-06-14 18:10:51 +0530

    Update longest palindromic  substring.cpp

--- week2/day5/longest palindromic  substring.cpp
+++ week2/day5/longest palindromic  substring.cpp
@@ -1,1 +1,40 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
 
+        int start = 0;
+        int maxLen = 1;
+
+        for (int i = 0; i < s.size(); i++) {
+
+          
+            int l = i, r = i;
+
+            while (l >= 0 && r < s.size() && s[l] == s[r]) {
+
+                if (r - l + 1 > maxLen) {
+                    maxLen = r - l + 1;
+                    start = l;
+                }
+
+                l--;
+                r++;
+            
+            l = i;
+            r = i + 1;
+
+            while (l >= 0 && r < s.size() && s[l] == s[r]) {
+
+                if (r - l + 1 > maxLen) {
+                    maxLen = r - l + 1;
+                    start = l;
+                }
+
+                l--;
+                r++;
+            }
+        }
+
+        return s.substr(start, maxLen);
+    }
+};
