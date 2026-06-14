17292957106219babe0831c7699de4c22f85ab23
shalini kumari <shalinikumari2898@gmail.com>
2026-06-14 18:03:54 +0530

    Update decode string.cpp

--- week2/day4/decode string.cpp
+++ week2/day4/decode string.cpp
@@ -1,1 +1,49 @@
+class Solution {
+public:
+    string decodeString(string s) {
 
+        stack<int> countStack;
+        stack<string> stringStack;
+
+        string currStr = "";
+        int num = 0;
+
+        for (char ch : s) {
+
+            if (isdigit(ch)) {
+                num = num * 10 + (ch - '0');
+            }
+
+            else if (ch == '[') {
+                countStack.push(num);
+                stringStack.push(currStr);
+
+                num = 0;
+                currStr = "";
+            }
+
+            else if (ch == ']') {
+
+                int repeat = countStack.top();
+                countStack.pop();
+
+                string prev = stringStack.top();
+                stringStack.pop();
+
+                string temp = "";
+
+                for (int i = 0; i < repeat; i++) {
+                    temp += currStr;
+                }
+
+                currStr = prev + temp;
+            }
+
+            else {
+                currStr += ch;
+            }
+        }
+
+        return currStr;
+    }
+};
